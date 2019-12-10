46725fb22bae5c829257064e41c26526f6e483ee
BPitts8019 <bradleyj.pitts@gmail.com>
2019-12-09 17:11:40 -0700

    project start

---
+++ day-03-crossed-wires/findClosestIntersection.js
@@ -0,0 +1,18 @@
+/**
+ * calculates postion points based on wire directions
+ * @param {array} wire 
+ */
+const calcPath = (wire) => {
+   let path = [];
+
+   //loop wire directions
+   //calc path
+
+   return path;
+};
+
+const findClosestIntersection = (blueWire, redWire) => {
+
+};
+
+module.exports = findClosestIntersection;
